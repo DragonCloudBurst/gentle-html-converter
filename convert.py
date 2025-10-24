e0a5972977be1bcb484493e3af4bbde04c2a9796
SPIVEYA23 <spiveya23@gmail.com>
2025-10-24 16:26:33 -0400

    begin building basic app structure, add .gitignore

--- convert.py
+++ convert.py
@@ -1,2 +1,16 @@
 from bs4 import BeautifulSoup
+import typer
+import lxml
 
+app = typer.Typer()
+
+@app.command()
+def parse(filename: str):
+    with open(filename, "r") as input_file:
+        html_text = input_file.read()
+        
+    print(html_text)
+    
+
+if __name__ == "__main__":
+    app()
