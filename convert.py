4fec695866e57897f65196a1b6e02df708e8b67a
SPIVEYA23 <spiveya23@gmail.com>
2025-12-05 15:52:26 -0500

    update README.md, clean up convert.py

--- convert.py
+++ convert.py
@@ -5,15 +5,14 @@ app = typer.Typer()
 @app.command()
 def parse(filename: str):
     with open(filename, "r") as input_file:
-        line_count = 0
         html_text_list = []
         
         for line in input_file:
             html_text_list.append(line)
-            line_count += 1
         
         html_text_full = input_file.read()
     
+    # insert javascript into file
     linetextadd1 = "                $a.pause();\n"
     linetextadd2 = "$a.addEventListener('click', () => { if ($a.paused) { $a.play(); } else { $a.pause(); } });\n"
     linetextadd3 = "#timebox { display: block; position: fixed; left: 60%; padding: 10px; margin: 5px; background-color: #666;\n"
@@ -26,6 +25,7 @@ def parse(filename: str):
     linetextadd10 = "var timeBox = document.getElementById(\"timebox\");\n"
     linetextadd11 = "	updateTimebox();\n"
     
+    # specific lines to insert to
     line_insert_to_idx_1 = 205
     line_insert_to_idx_2 = 140
     line_insert_to_idx_3 = 104
@@ -56,7 +56,6 @@ def parse(filename: str):
     with open(f"files/{bare_file_title}_parsed.html" , "w") as new_html_file:
         new_html_file.writelines(html_text_list)
     new_html_file.close()
-    
     
 if __name__ == "__main__":
     app()
